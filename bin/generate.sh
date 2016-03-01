fd33fdde2e0b7107b680b5365944149342eae2be
Ghislain Rodrigues <git@ghislain-rodrigues.fr>
2016-03-01 12:48:29 +0000

    script to generate the html added

---
+++ bin/generate.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+
+FILES=`find src/ -name "*.md"`
+
+for f in $FILES
+do
+	DEST=`echo "$f" | sed 's#^src/#web/#' | sed 's#.md$#.html#'`
+	DESTTMP="$DEST.tmp"
+	mkdir -p `dirname "$DEST"`
+	markdown "$f" > "$DESTTMP"
+	cat src/header.part "$DESTTMP" src/footer.part > "$DEST"
+	rm "$DESTTMP"
+done
