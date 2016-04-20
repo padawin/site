9fc08f009e286d9add63b3025a90ca945f31fa38
Ghislain Rodrigues <git@ghislain-rodrigues.fr>
2016-04-20 21:55:03 +0100

    the screen sizes are now provided in the module

--- web-game/screen-size.js
+++ web-game/screen-size.js
@@ -1,7 +1,26 @@
-loader.executeModule('screenSize', 'B', function (B) {
+loader.addModule('screenSize', 'B', function (B) {
 	"use strict";
 
-	window.addEventListener('resize', function () {
+	var screenSizeValues,
+		screenSize = {
+			get: function () {
+				return screenSizeValues;
+			}
+		};
+
+	function updateScreenSize () {
+		screenSizeValues = {
+			x: Math.max(document.documentElement.clientWidth, window.innerWidth || 0),
+			y: Math.max(document.documentElement.clientHeight, window.innerHeight || 0)
+		};
+	}
+
+	updateScreenSize();
+
+	window.addEventListener('resize', function (e) {
+		updateScreenSize();
 		B.Events.fire('resize');
 	}, false);
+
+	return screenSize;
 });
