8382c1541381b419f345143fe9f8509f707b2e96
Ghislain Rodrigues <git@ghislain-rodrigues.fr>
2016-04-22 15:34:07 +0100

    clouds added in the sky as particles

--- web-game/sky.js
+++ web-game/sky.js
@@ -1,9 +1,10 @@
-loader.addModule('sky', function () {
+loader.addModule('sky', 'particles', function (particlesModule) {
 	"use strict";
 	var sky = {
 			nbResources: 1
 		},
-		cloudResource;
+		cloudResource,
+		clouds = new particlesModule.ParticlesManager(20);
 
 	sky.loadResources = function (loaded) {
 		cloudResource = new Image();
@@ -12,7 +13,19 @@ loader.addModule('sky', function () {
 	}
 
 	sky.update = function () {
+		var frequencyCloud = Math.random(),
+			cloud,
+			cloudDimensions;
 
+		if (frequencyCloud > 0.9 && !clouds.isFull()) {
+			cloud = new particlesModule.Particle(
+				{x: -cloudResource.width, y: Math.random()},
+				{x: Math.random() * 2, y: 0},
+				10000
+			);
+			cloud.ratio = Math.max(0.2, Math.random());
+			clouds.addParticle(cloud);
+		}
 	};
 
 	sky.draw = function (canvasContext, camera) {
