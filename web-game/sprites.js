15f0c79fe73e16692e126ab6b2f6094e00a771c7
Ghislain Rodrigues <git@ghislain-rodrigues.fr>
2016-04-25 13:12:55 +0100

    comments added

--- web-game/sprites.js
+++ web-game/sprites.js
@@ -7,8 +7,11 @@ loader.addModule('sprites', function () {
 				PLAYER: 3
 			},
 			sprites: [
+				// grass
 				{x: 0, y: 0, w: 64, h: 36, d: 73, walkable: true},
+				// stone
 				{x: 64, y: 0, w: 64, h: 36, d: 73, walkable: true},
+				// stairs
 				{x: 128, y: 0, w: 64, h: 36, d: 73, walkable: true},
 				// player
 				{x: 0, y: 73, w: 64, h: 64, posInCell: {x: 32, y: 44}},
