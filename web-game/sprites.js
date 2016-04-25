aee30a1350e0c9571d4ddf6680169dacd6a90a0a
Ghislain Rodrigues <git@ghislain-rodrigues.fr>
2016-04-25 13:05:01 +0100

    sprite module completed

--- web-game/sprites.js
+++ web-game/sprites.js
@@ -1,7 +1,19 @@
 loader.addModule('sprites', function () {
-	var sprites = [
-		{x: 0, y: 0, w: 64, h: 36, d: 73, walkable: true},
-		{x: 64, y: 0, w: 64, h: 36, d: 73, walkable: true},
-		{x: 128, y: 0, w: 64, h: 36, d: 73, walkable: true}
-	];
+	var spriteBoardUrl = 'sprite.png',
+		spriteModule = {
+			nbResources: 1,
+			spriteResource: null,
+			sprites: [
+				{x: 0, y: 0, w: 64, h: 36, d: 73, walkable: true},
+				{x: 64, y: 0, w: 64, h: 36, d: 73, walkable: true},
+				{x: 128, y: 0, w: 64, h: 36, d: 73, walkable: true}
+			],
+			loadResources: function (loaded) {
+				spriteModule.spriteResource = new Image();
+				spriteModule.spriteResource.onload = loaded;
+				spriteModule.spriteResource.src = spriteBoardUrl;
+			}
+		};
+
+	return spriteModule;
 });
