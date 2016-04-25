344785faa805d57297f66c6073774a002fa009ed
Ghislain Rodrigues <git@ghislain-rodrigues.fr>
2016-04-25 07:50:59 +0100

    click event registered only if the mouse did not move between mousedown and click

--- web-game/mouse.js
+++ web-game/mouse.js
@@ -1,7 +1,25 @@
 loader.addModule('mouse', 'B', 'canvas', function (B, canvas) {
-	var isMouseDown = false;
+	var isMouseDown = false,
+		isMouseMoving = false;
+
+	canvas.canvas.addEventListener('mousedown', function (event) {
+		isMouseDown = true;
+	});
+
+	canvas.canvas.addEventListener('mousemove', function (event) {
+		if (isMouseDown) {
+			isMouseMoving = true;
+		}
+	});
 
 	canvas.canvas.addEventListener('click', function (event) {
+		isMouseDown = false;
+
+		if (isMouseMoving) {
+			isMouseMoving = false;
+			return;
+		}
+
 		var rect = canvas.canvas.getBoundingClientRect(),
 			root = document.documentElement,
 			mouseX = event.clientX - rect.left - root.scrollLeft,
