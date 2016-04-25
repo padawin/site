165265fb0644946b96995f6fc492df6c2bec6faa
Ghislain Rodrigues <git@ghislain-rodrigues.fr>
2016-04-25 08:44:48 +0100

    sprites module added

---
+++ web-game/sprites.js
@@ -0,0 +1,7 @@
+loader.addModule('sprites', function () {
+	var sprites = [
+		{x: 0, y: 0, w: 64, h: 36, d: 73, walkable: true},
+		{x: 64, y: 0, w: 64, h: 36, d: 73, walkable: true},
+		{x: 128, y: 0, w: 64, h: 36, d: 73, walkable: true}
+	];
+});
