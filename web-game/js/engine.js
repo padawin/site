6a879d38901f44947194aed2a7c04cdfb85fff83
Ghislain Rodrigues <git@ghislain-rodrigues.fr>
2016-04-28 13:31:41 +0100

    method to calculate a distance

--- web-game/js/engine.js
+++ web-game/js/engine.js
@@ -282,6 +282,10 @@ function (B, sky, canvas, sprites, pathFinding) {
 		);
 	}
 
+	function calcDistance (p1, p2) {
+		return Math.sqrt(Math.pow(p2.x - p1.x, 2) + Math.pow(p2.y - p1.y, 2));
+	}
+
 	function loadResources (callback) {
 		// sprite + sky, will evolve
 		var nbResources = sprites.nbResources + sky.nbResources,
