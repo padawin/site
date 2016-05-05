98a19f9a9c3738740505a6c3d5ff0d48d838ef6f
Ghislain Rodrigues <git@ghislain-rodrigues.fr>
2016-05-05 22:27:05 +0100

    provide an object directly to the inventory method

--- web-game/js/inventory.js
+++ web-game/js/inventory.js
@@ -6,17 +6,19 @@ function (B) {
 	var _content = [],
 		inventory = {};
 
-	inventory.add = function (name, icon, action) {
-		_content.push({name: name, icon: icon});
+	inventory.add = function (object) {
+		_content.push(object);
 		var span, li = B.create(
 			'li',
 			{'data-index': _content.length},
 			'inventory-list'
 		);
-		B.create('img', {src: icon, alt: name}, li);
-		span = B.create('span', {text: name}, li);
+		B.create('img', {src: object.icon, alt: object.name}, li);
+		span = B.create('span', {text: object.name}, li);
 
-		B.on(span, 'click', action);
+		if (object.onopen) {
+			B.on(span, 'click', object.onopen);
+		}
 	};
 
 	B.on('open-inventory', 'click', function () {
