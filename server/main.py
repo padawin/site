5680fd246e151b8876b7f03e542e0d5ec6ad3b64
Ghislain Rodrigues <git@ghislain-rodrigues.fr>
2016-05-10 21:28:34 +0100

    quotes harmonisation

--- server/main.py
+++ server/main.py
@@ -18,18 +18,18 @@ class MainHandler(tornado.web.RequestHandler):
 		except ValueError:
 			self.clear()
 			self.set_status(400)
-			self.finish({"error": 'missing parameter from and message'})
+			self.finish({"error": "missing parameter from and message"})
 			return
 
-		if 'from' not in data.keys():
+		if "from" not in data.keys():
 			self.clear()
 			self.set_status(400)
-			self.finish({"error": 'missing parameter from'})
+			self.finish({"error": "missing parameter from"})
 			return
-		elif 'message' not in data.keys():
+		elif "message" not in data.keys():
 			self.clear()
 			self.set_status(400)
-			self.finish({"error": 'missing parameter message'})
+			self.finish({"error": "missing parameter message"})
 			return
 
 		# Create a text/plain message
@@ -37,42 +37,42 @@ class MainHandler(tornado.web.RequestHandler):
 
 		me = "contact@ghislain-rodrigues.fr"
 
-		msg['Subject'] = 'Contact from website from %s' % data["from"]
-		msg['From'] = data["from"]
-		msg['To'] = me
+		msg["Subject"] = "Contact from website from %s" % data["from"]
+		msg["From"] = data["from"]
+		msg["To"] = me
 
-		if not validate_email(data['from']):
+		if not validate_email(data["from"]):
 			self.clear()
 			self.set_status(400)
-			self.finish({"error": 'invalid email address'})
+			self.finish({"error": "invalid email address"})
 			syslog.syslog(syslog.LOG_NOTICE, "mail website,invalid email address,from %s" % data["from"])
 			return
 
 
 		result = {}
-		s = smtplib.SMTP('localhost')
+		s = smtplib.SMTP("localhost")
 		try:
 			s.sendmail(me, [me], msg.as_string())
-			result['result'] = 'ok'
+			result["result"] = "ok"
 		except SMTPRecipientsRefused:
-			result['result'] = 'ko'
-			result['code'] = 1
-			result['reason'] = 'All recipients were refused'
+			result["result"] = "ko"
+			result["code"] = 1
+			result["reason"] = "All recipients were refused"
 			syslog.syslog(syslog.LOG_NOTICE, "mail website,exception SMTPRecipientsRefused,from %s" % data["from"])
 		except SMTPHeloError:
-			result['result'] = 'ko'
-			result['code'] = 2
-			result['reason'] = 'server error'
+			result["result"] = "ko"
+			result["code"] = 2
+			result["reason"] = "server error"
 			syslog.syslog(syslog.LOG_NOTICE, "mail website,exception SMTPHeloError,from %s" % data["from"])
 		except SMTPSenderRefused:
-			result['result'] = 'ko'
-			result['code'] = 3
-			result['reason'] = 'invalid from address'
+			result["result"] = "ko"
+			result["code"] = 3
+			result["reason"] = "invalid from address"
 			syslog.syslog(syslog.LOG_NOTICE, "mail website,exception SMTPSenderRefused,from %s" % data["from"])
 		except SMTPDataError:
-			result['result'] = 'ko'
-			result['code'] = 4
-			result['reason'] = 'unexpected error. good luck with that'
+			result["result"] = "ko"
+			result["code"] = 4
+			result["reason"] = "unexpected error. good luck with that"
 			syslog.syslog(syslog.LOG_NOTICE, "mail website,exception SMTPDataError,from %s" % data["from"])
 		s.quit()
 
