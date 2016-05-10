5aed1657cc5f7b5cd3e35f3d90c13ed5d442c488
Ghislain Rodrigues <git@ghislain-rodrigues.fr>
2016-05-10 21:46:01 +0100

    js added for the contact form

---
+++ web-game/js/contactForm.js
@@ -0,0 +1,35 @@
+loader.executeModule('contactForm', 'B', function (B) {
+	"use strict";
+	/**
+	 * Module to set the events on the contact form and send a request
+	 * when the form is submitted
+	 */
+
+	var form = B.$id('contact-form');
+	B.on(form, 'submit', function (e) {
+		e.preventDefault();
+
+		var data = {
+			from: form.from.value,
+			message: form.message.innerText
+		};
+		B.Ajax.request(
+			'http://localhost:8888/contact',
+			{
+				200: function () {
+					console.log('200');
+					console.log(arguments);
+				},
+				400: function () {
+					console.log('400');
+					console.log(arguments);
+				},
+				500: function () {
+					console.log('500');
+					console.log(arguments);
+				}
+			}, null, 'POST', JSON.stringify(data)
+		);
+	});
+});
+
