f7df5c5356f4366dd2aa0baa515838962a79bd4b
Ghislain Rodrigues <git@ghislain-rodrigues.fr>
2017-10-08 22:32:20 +0100

    floodit image added for the inventory

--- src-game/js/level.js
+++ src-game/js/level.js
@@ -138,7 +138,7 @@ loader.addModule('level', 'sprites', function (sprites) {
 			{x: 25, y: 8, sprite: sprites.sprites[sprites.SPRITES_ACCESS.TREE]},
 			{x: 32, y: 5, sprite: sprites.sprites[sprites.SPRITES_ACCESS.TREE_LARGE]},
 			{x: 25, y: 3, sprite: sprites.sprites[sprites.SPRITES_ACCESS.FLOODIT],
-				canBeTaken: true, name: 'Floodit', icon: '/images/floodit.png',
+				canBeTaken: true, name: 'Floodit', icon: 'images/floodit.png',
 				onopen: function () {
 					window.open('https://github.com/padawin/floodit-clone');
 				}
